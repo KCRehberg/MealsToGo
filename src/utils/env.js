1ebcfd3d659d176bd5f985a5b2b2d86a9b33b84a
KCRehberg <38145033+KCRehberg@users.noreply.github.com>
2021-03-20 13:02:47 -0400

    Delete env.js

--- src/utils/env.js
+++
@@ -1,4 +0,0 @@
-const liveHost = 'https://us-central1-mealstogo-ba553.cloudfunctions.net';
-const localHost = 'http://localhost:5001/mealstogo-ba553/us-central1';
-export const isDevelopment = process.env.NODE_ENV === 'development';
-export const host = liveHost;
